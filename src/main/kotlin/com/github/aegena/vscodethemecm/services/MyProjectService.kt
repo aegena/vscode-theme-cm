5a170c04cc33082075d0c5a638a7a019f630651f
GitHub Action <action@github.com>
2024-04-25 15:35:44 +0000

    Template cleanup

--- src/main/kotlin/com/github/aegena/vscodethemecm/services/MyProjectService.kt
+++ src/main/kotlin/com/github/aegena/vscodethemecm/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.aegena.vscodethemecm.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.aegena.vscodethemecm.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
